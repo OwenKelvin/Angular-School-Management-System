308aeab3f9d4b0d79b195c1292344039750fdc7d
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-12-05 13:55:37 +0300

    Fixed travis CI Failing

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
