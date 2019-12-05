59eb948966a5be5a2069b5409f34d137f5b6f9d7
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-12-05 22:03:32 +0300

    Test coverage on authentication service

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -5,16 +5,38 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
 import { reducer } from 'src/app/store/reducers';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthenticationService } from '../services/authentication/authentication.service';
 
 describe('AuthGuard', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule, StoreModule.forRoot(reducer)],
       providers: [AuthGuard]
     });
-  });
+  }));
 
   it('should create auth guard', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
+  }));
+  it('should return true if current user ', () => {
+    const next = jasmine.createSpyObj({queryParams: ''});
+    const state = jasmine.createSpyObj({url: ''});
+    const router = jasmine.createSpyObj({ navigate: () => { } });
+    const authenticationServive = jasmine.createSpyObj({
+      currentUserValue: true
+    });
+    const authGuard = new AuthGuard(router, authenticationServive);
+    expect(authGuard.canActivate(next, state)).toBeTruthy();
+  });
+  it('should return false if no current user ', inject([AuthenticationService], (authenticationServive: AuthenticationService) => {
+    const next = jasmine.createSpyObj({queryParams: ''});
+    const state = jasmine.createSpyObj({url: ''});
+    const router = jasmine.createSpyObj({ navigate: () => { } });
+    const auth: AuthenticationService = Object.create(authenticationServive, {
+      currentUserValue : { value: false}
+    });
+    const authGuard = new AuthGuard(router, auth);
+    expect(authGuard.canActivate(next, state)).toBeFalsy();
+    expect(router.navigate).toHaveBeenCalled();
   }));
 });
--- src/app/core/services/authentication/authentication.service.spec.ts
+++ src/app/core/services/authentication/authentication.service.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+
 
 
 describe('AuthenticateService', () => {
@@ -14,13 +16,31 @@ describe('AuthenticateService', () => {
     const service: AuthenticationService = TestBed.get(AuthenticationService);
     expect(service).toBeTruthy();
   });
-  // it('should have a login function that calls http with correct parameters', () => {
+  it('should have a login function that calls http with correct parameters', inject([HttpClient], (http: HttpClient) => {
+    const store = jasmine.createSpyObj({ dispatch: () => { } });
+    const mockHttp = Object.create(http);
+    const config = jasmine.createSpyObj({ post: () => { } });
+    const spy = spyOn(mockHttp, 'post').and.returnValue({pipe: () => {}});
+    const service = new AuthenticationService(store, mockHttp, config);
+    service.login({ username: 'user', password: 'password', rememberMe: true });
+    expect(spy).toHaveBeenCalled();
+  }));
+  // it('should have a login function that calls http with correct parameters', inject([HttpClient], (http: HttpClient) => {
   //   const store = jasmine.createSpyObj({ dispatch: () => { } });
-  //   // tslint:disable-next-line:arrow-return-shorthand
-  //   const http = jasmine.createSpyObj(new HttpClient({handle: jasmine.createSpy()}));
+  //   const mockHttp = Object.create(http);
   //   const config = jasmine.createSpyObj({ post: () => { } });
-  //   const service = new AuthenticationService(store, http, config);
-  //   service.login({ username: 'user', password: 'password', rememberMe: true });
-  //   expect(http.post).toHaveBeenCalled();
-  // });
+  //   const service = new AuthenticationService(store, mockHttp, config);
+  //   const spy = spyOnProperty(service, 'currentUserSubject');
+  //   service.logout();
+  //   expect(spy).toHaveBeenCalled();
+  // }));
+  it('should have a login function that calls http with correct parameters', inject([HttpClient], (http: HttpClient) => {
+    const store = jasmine.createSpyObj({ dispatch: () => { } });
+    const mockHttp = Object.create(http);
+    const config = jasmine.createSpyObj({ post: () => { } });
+    const spy = spyOn(mockHttp, 'post').and.returnValue({pipe: () => {}});
+    const service = new AuthenticationService(store, mockHttp, config);
+    service.logout();
+    expect(service.authorizationToken()).toBeTruthy();
+  }));
 });
